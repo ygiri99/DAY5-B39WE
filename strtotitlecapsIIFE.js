211f042e3d69ef8016029095ef602ba3e39b387f
ygiri99 <110931485+ygiri99@users.noreply.github.com>
2022-09-18 22:16:47 +0530

    Update strtotitlecapsIIFE.js

--- strtotitlecapsIIFE.js
+++ strtotitlecapsIIFE.js
@@ -1,9 +1,16 @@
-const arr = ['Ananth','Arasu','Aruvi','Balaji','Babu','Nagaraj'],
+const arr = ['ananth','arasu','aruvi','balaji','babu','nagaraj'],
 strArr = [];
 //IIFE
 (function(){
     for(var ele of arr){
-        strArr.push(ele.toUpperCase());
+       let string = "";
+    for(var i=0;i<ele.length;i++){
+        if(i===0)
+            string+=ele[i].toUpperCase();
+        else
+            string+=ele[i].toLowerCase();
+    }
+    strArr.push(string);
     }
 })();
 //anonymousfun();
