e2d0b6cea8408a1fb650f0f68841ae3982163656
ygiri99 <110931485+ygiri99@users.noreply.github.com>
2022-09-18 22:22:08 +0530

    Update strtolittlecapsIIFE.js

--- strtolittlecapsIIFE.js
+++ strtolittlecapsIIFE.js
@@ -1,16 +1,9 @@
-const arr = ['ananth','arasu','aruvi','balaji','babu','nagaraj'],
+const arr = ['Ananth','ARasu','Aruvi','Balaji','baBu','nagarRAJ'];
 strArr = [];
 //IIFE
 (function(){
     for(var ele of arr){
-       let string = "";
-    for(var i=0;i<ele.length;i++){
-        if(i===0)
-            string+=ele[i].toUpperCase();
-        else
-            string+=ele[i].toLowerCase();
-    }
-    strArr.push(string);
+        strArr.push(ele.toLowerCase());
     }
 })();
 //anonymousfun();
