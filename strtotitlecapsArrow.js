f9fff62d50a2c0eba7fb7401fce87f4627b5fc12
ygiri99 <110931485+ygiri99@users.noreply.github.com>
2022-09-18 22:09:13 +0530

    Update strtotitlecapsArrow.js

--- strtotitlecapsArrow.js
+++ strtotitlecapsArrow.js
@@ -1,7 +1,14 @@
-const arr = ['Ananth','Arasu','Aruvi','Balaji','Babu','Nagaraj'],
+const arr = ['ananth','arasu','aruvi','balaji','babu','nagaraj'],
 strArr = [];
 // Arrow 
 arr.forEach((str) =>{
-    strArr.push(str.toUpperCase());
+    let string = "";
+    for(var i=0;i<str.length;i++){
+        if(i===0)
+            string+=str[i].toUpperCase();
+        else
+            string+=str[i].toLowerCase();
+    }
+    strArr.push(string);
 });
 console.log(strArr);
